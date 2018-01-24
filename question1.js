052eb4987c675524e19cbaf7b015cbab54b96a5c
humzahchoudry <Humzah.choudry@gmail.com>
2018-01-23 19:01:13 -0500

    added css and updated index

---
+++ question1.js
@@ -0,0 +1,13 @@
+<div class="question-1">
+  <div class="question">
+    <h2>Question 1: </h2>
+  </div>
+  <div class="row ">
+    <button type="button" name="answer-one">Answer One</button>
+    <button type="button" name="answer-two">Answer Two</button>
+  </div>
+  <div class="row">
+    <button type="button" name="answer-three">Answer Three</button>
+    <button type="button" name="answer-four">Answer Four</button>
+  </div>
+</div>
